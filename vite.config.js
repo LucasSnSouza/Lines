9a8faa19f60af44a28c78aaffff1efc9c21495cb
LucasSnSouza <lucassantospublico@gmail.com>
2024-06-15 18:04:25 -0300

    fix / make pwa

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,15 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  pwa: {
+    name: 'My App',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    workboxOptions: {
+      navigateFallback: 'index.html',
+    }
   }
 })
